47bc3345f132395c3920a7b5f7d27f480bcf9bde
kooriblue <40480185+kooriblue@users.noreply.github.com>
2018-12-11 11:50:35 +0800

    添加了获取最新帖子

--- back-end/koori/route.php
+++ back-end/koori/route.php
@@ -30,18 +30,25 @@ Route::rule('/personalcenter/getAllInfo', 'index/Personalcenter/getAllInfo');
 Route::rule('/personalcenter/modify', 'index/Personalcenter/modify');
 
 //messageboard
+//Route::rule('Cblog/index', 'index/Cblog/index');
 Route::rule('Cblog/stack', 'index/Cblog/stack');
 Route::rule('Cblog/blog', 'index/Cblog/blog');
 Route::rule('Cblog/post', 'index/Cblog/post');
+
 Route::rule('/cblog/getBlogList', 'index/Cblog/getBlogList');
-Route::rule('/cblog/getIndexList', 'index/Cblog/getIndexList');
+//Route::rule('/cblog/getIndexList', 'index/Cblog/getIndexList');
+Route::rule('/cblog/getNew', 'index/Cblog/getNew');
+
 Route::rule('/cblog/enter', 'index/Cblog/enter');
 Route::rule('/cblog/getBlog', 'index/Cblog/getBlog');
+
 Route::rule('/cblog/addBlog', 'index/Cblog/addBlog');
 Route::rule('/cblog/modifyBlog', 'index/Cblog/modifyBlog');
 Route::rule('/cblog/deleteBlog', 'index/Cblog/deleteBlog');
+
 Route::rule('/cblog/collect', 'index/Cblog/collect');
 Route::rule('/cblog/cancelCollect', 'index/Cblog/cancelCollect');
+
 Route::rule('/cblog/replyComment', 'index/Cblog/replyComment');
 Route::rule('/cblog/modifyComment', 'index/Cblog/modifyComment');
 Route::rule('/cblog/deleteComment', 'index/Cblog/deleteComment');
